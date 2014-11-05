65ab226743bbe40909eab9690b8d948e0895ac17
akalke <amaeya.kalke@gmail.com>
2014-11-05 09:22:13 -0600

    Initial Commit

---
+++ Stockd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stockd
+//
+//  Created by Amaeya Kalke on 11/5/14.
+//  Copyright (c) 2014 Amaeya Kalke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Stockd/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Stockd
+//
+//  Created by Amaeya Kalke on 11/5/14.
+//  Copyright (c) 2014 Amaeya Kalke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
