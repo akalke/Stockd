4cfe4999456223447141890c87eff42072bc7fc2
aduflo <adduflo@gmail.com>
2014-11-05 15:33:27 -0500

    Working on storesnearby

--- Stockd/StoresNearbyViewController.m
+++ Stockd/StoresNearbyViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "StoresNearbyViewController.h"
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
 
-@interface StoresNearbyViewController ()
+@interface StoresNearbyViewController () <MKMapViewDelegate, CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property CLLocationManager *locationManager;
 
 @end
 
@@ -16,22 +21,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    [self.locationManager requestWhenInUseAuthorization];
+    self.locationManager.delegate = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - MapView Methods
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    if (annotation == mapView.userLocation) {
+        return nil;
+    }
+    
+    MKAnnotationView *annotationView = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"Pin"];
+    
+    return annotationView;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view {
+    
 }
-*/
+
+#pragma mark - LocationManager Methods
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"Failure Error: %@", error);
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    for (CLLocation *location in locations) {
+        if (location.verticalAccuracy < 1000 && location.horizontalAccuracy < 1000) {
+            self.textView.text = @"Found Your Location";
+            
+            [self reverseGeocode:location];
+            [self.locationManager stopUpdatingLocation];
+            break;
+        }
+    }
+}
+
+#pragma mark - Helper Methods
+
+- (void)reverseGeocode:(CLLocation *)location {
+    CLGeocoder *geocoder = [CLGeocoder new];
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        CLPlacemark *placemark = placemarks.firstObject;
+        NSString *address = [NSString stringWithFormat:@"%@ %@, %@, %@", placemark.subThoroughfare, placemark.thoroughfare, placemark.locality, placemark.administrativeArea];
+        self.textView.text = [NSString stringWithFormat:@"Your location: %@", address];
+    }];
+}
+
+- (IBAction)onButtonPressed:(id)sender {
+    [self.locationManager startUpdatingLocation];
+    self.textView.text = @"Locating Nearby Stores";
+}
 
 @end
